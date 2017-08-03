6d769c62248fbf1f3a628db6f8363ca3f8441e08
Alfonso Diaz <aldiaz@github.com.br>
2017-08-03 18:14:50 -0300

    Initial structure with spring boot

---
+++ src/main/java/com/dc/gth/batplatform/BatplatformApplication.java
@@ -0,0 +1,12 @@
+package com.dc.gth.batplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatplatformApplication.class, args);
+	}
+}
